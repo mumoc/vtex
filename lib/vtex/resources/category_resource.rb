fcf4b5395cae507cf969b553f23357db3205f857
David Sanchez <sanchez.dav90@gmail.com>
2017-08-02 13:42:18 -0500

    Category tree endpoint resource  was added

--- lib/vtex/resources/category_resource.rb
+++ lib/vtex/resources/category_resource.rb
@@ -4,6 +4,10 @@ module Vtex
       action :by_id, 'GET /api/catalog_system/pvt/category/:id' do
         handler(200) { |response| CategoryMapping.extract_single(response.body, :read) }
       end
+
+      action :tree, 'GET /api/catalog_system/pub/category/tree/:levels/' do
+        handler(200) { |response| CategoryMapping.extract_collection(response.body, :read) }
+      end
     end
   end
 end
