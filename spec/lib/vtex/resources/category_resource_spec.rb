608ef770ee159456519c5628667208f9946b6344
David Sanchez <sanchez.dav90@gmail.com>
2017-08-02 13:37:55 -0500

    Category tree endpoint test was added

--- spec/lib/vtex/resources/category_resource_spec.rb
+++ spec/lib/vtex/resources/category_resource_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Vtex::CategoryResource do
       expect(category).to be_kind_of(Vtex::Category)
     end
   end
+
+  describe "#tree" do
+    it "returns tree of categories" do
+      stub_do_api('/api/catalog_system/pub/category/tree/2/', :get).to_return(body: api_fixture('categories/tree'))
+      tree = resource.tree(levels: 2)
+      expect(tree).to be(tree)
+    end
+  end
 end
 
