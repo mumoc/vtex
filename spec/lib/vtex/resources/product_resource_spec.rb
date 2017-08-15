897288731b315f03ac4da925c59321dda9ebbbbe
David Sanchez <sanchez.dav90@gmail.com>
2017-08-15 17:44:56 -0500

    Product resource spec
    Action by_id was modified to product_by_id

--- spec/lib/vtex/resources/product_resource_spec.rb
+++ spec/lib/vtex/resources/product_resource_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Vtex::ProductResource do
   subject(:resource) { described_class.new(connection: connection) }
   include_context 'resources'
 
-  describe "#by_id" do
+  describe "#product_by_id" do
     it "returns a single product" do
-      stub_do_api('/api/catalog_system/pvt/products/ProductGet/1', :get).to_return(body: api_fixture('products/by_id'))
-      product = resource.by_id(id: 1)
+      stub_do_api('/api/catalog_system/pvt/products/ProductGet/1', :get).to_return(body: api_fixture('product/product_by_id'))
+      product = resource.product_by_id(id: 1)
       expect(product).to be_kind_of(Vtex::Product)
     end
   end
