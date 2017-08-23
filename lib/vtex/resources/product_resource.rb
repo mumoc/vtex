366fcec16772ec52dc394ece82f8156f59fcf745
David Sanchez <sanchez.dav90@gmail.com>
2017-08-23 18:21:56 -0500

    Product and Sku Ids action
    Was fixed with query params

--- lib/vtex/resources/product_resource.rb
+++ lib/vtex/resources/product_resource.rb
@@ -9,8 +9,8 @@ module Vtex
         handler(200) { |response| ProductMapping.extract_single(response.body, :read) }
       end
 
-      action :product_and_sku_ids, 'GET /api/catalog_system/pvt/products/GetProductAndSkuIds?categoryId=#{category_id}&_from=#{from}&_to=#{to}' do
-        handler(200) { |response| ProductAndSkuIdsMapping.extract_single(response.body, :read) }
+      action :product_and_sku_ids, "GET /api/catalog_system/pvt/products/GetProductAndSkuIds?categoryId=:category_id&_from=:from&_to=:to" do
+        handler(200) { |response| response.body }
       end
     end
   end
