66568868203a13428c2bda26ae037c9be02dfd6a
Alberto Mendoza <albertm_805@outlook.com>
2017-08-23 19:22:16 -0500

    added tests and fixed resource

--- lib/vtex/resources/indexer_changenotification_and_autocomplete_resource.rb
+++ lib/vtex/resources/indexer_changenotification_and_autocomplete_resource.rb
@@ -1,10 +1,9 @@
 module Vtex
   class IndexerNotificationAndAutocompleteResource < ResourceKit::Resource
-    resource do
-      action :change_notification, 'GET /api/catalog_system/pvt/skuseller/changenotification/{{productId}}' do
-        handler(200) {|response| ProductMapping.extract_single(response.body, :read) }
+    resources do
+      action :change_notification, 'GET /api/catalog_system/pvt/skuseller/changenotification/:product_id' do
+        handler(200) { |response| response.body }
+      end
     end
   end
 end
-
-
