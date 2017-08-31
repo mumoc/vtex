daec5e076014ca79ea7a2ac87b4a93e18ddfba07
David Sanchez <sanchez.dav90@gmail.com>
2017-08-30 21:20:49 -0500

    Sales Channel Spec
    Sales Channel Resource spec created

---
+++ spec/lib/vtex/resources/sales_channel_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe Vtex::SalesChannelResource do
+  subject(:resource) { described_class.new(connection: connection) }
+  include_context 'resources'
+
+  describe "#sales_channel_list" do
+    it "returns a sales channel array" do
+      stub_do_api('/api/catalog_system/pvt/saleschannel/list', :get).to_return(body: api_fixture('sales_channel/sales_channel_list'))
+      sales = resource.sales_channel_list
+      expect(sales).to be_kind_of(Vtex::SaleChannel)
+    end
+  end
+end
