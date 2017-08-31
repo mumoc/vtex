61cb99d7318438bfc722c6dca67bd80c479c82ce
David Sanchez <sanchez.dav90@gmail.com>
2017-08-30 21:24:34 -0500

    Test
    Test passed

--- lib/vtex/resources/sales_channel_resource.rb
+++ lib/vtex/resources/sales_channel_resource.rb
@@ -2,7 +2,7 @@ module Vtex
   class SalesChannelResource < ResourceKit::Resource
     resources do
       action :sales_channel_list, 'GET /api/catalog_system/pvt/saleschannel/list' do
-        handler(200) { |response| SaleChannelMapping.extract_single(response.body, :read) }
+        handler(200) { |response| SaleChannelMapping.extract_collection(response.body, :read) }
       end
     end
   end
--- spec/lib/vtex/resources/sales_channel_spec.rb
+++ spec/lib/vtex/resources/sales_channel_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Vtex::SalesChannelResource do
     it "returns a sales channel array" do
       stub_do_api('/api/catalog_system/pvt/saleschannel/list', :get).to_return(body: api_fixture('sales_channel/sales_channel_list'))
       sales = resource.sales_channel_list
-      expect(sales).to be_kind_of(Vtex::SaleChannel)
+      expect(sales).to match_array(Vtex::SaleChannel)
     end
   end
 end
