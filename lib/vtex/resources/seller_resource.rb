eb9c8b36fc00d0d14dc13f19f902dedb8d9fffd0
Alberto Mendoza <albertm_805@outlook.com>
2017-09-01 12:13:50 -0500

    added resource

---
+++ lib/vtex/resources/seller_resource.rb
@@ -0,0 +1,8 @@
+module Vtex
+  class SellerResource < ResourceKit::Resource
+    resources do
+      action :seller_list, "GET api/catalog_system/pvt/seller/list" do
+        handler(200) { |response| SellerListMapping.extract_single(response.body, :read) }
+      end
+    end
+  end
