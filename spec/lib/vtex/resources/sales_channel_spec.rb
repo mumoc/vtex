0690e4f51babf1af0bd2a5196845ad5fdcc7b8f0
David Sanchez <sanchez.dav90@gmail.com>
2017-09-05 11:52:07 -0500

    Spec describe
    sales_channel_by_id describe was added into sales channel resource spec

--- spec/lib/vtex/resources/sales_channel_spec.rb
+++ spec/lib/vtex/resources/sales_channel_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Vtex::SalesChannelResource do
       expect(sales).to match_array(Vtex::SaleChannel)
     end
   end
+
+  describe "#sales_channel_by_id" do
+    it "returns a single sales channel" do
+      stub_do_api('/api/catalog_system/pub/saleschannel/1', :get).to_return(body: api_fixture('sales_channel/sales_channel_by_id'))
+      sales = resource.sales_channel_by_id(sales_channel_id: 1)
+      expect(sales).to be_kind_of(Vtex::SaleChannel)
+    end
+  end
 end
