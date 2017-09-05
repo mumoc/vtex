5c6a9ee85895314a8c47a362549e11ad5b5d1104
David Sanchez <sanchez.dav90@gmail.com>
2017-09-05 11:39:11 -0500

    Revert "Sales Channel By Id"
    This reverts commit 7a0c194d52279a0565b5238c01c9aa51c28bb447.

--- lib/vtex/resources/sales_channel_resource.rb
+++ lib/vtex/resources/sales_channel_resource.rb
@@ -4,10 +4,6 @@ module Vtex
       action :sales_channel_list, 'GET /api/catalog_system/pvt/saleschannel/list' do
         handler(200) { |response| SaleChannelMapping.extract_collection(response.body, :read) }
       end
-
-      action :sales_channel_by_id, 'GET /api/catalog_system/pub/saleschannel/:sales_channel_id' do
-        handler(200) { |response| SaleChannelMapping.extract_single(response.body, :read) }
-      end
     end
   end
 end
