718a3a081c1b20c75114ac2294233ab92d123c0c
David Sanchez <sanchez.dav90@gmail.com>
2017-09-05 17:17:13 -0500

    Spec
    Sku resource spec was fixed

--- spec/lib/vtex/resources/sku_resource_spec.rb
+++ spec/lib/vtex/resources/sku_resource_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Vtex::Sku do
+RSpec.describe Vtex::SkuResource do
   subject(:resource) { described_class.new(connection: connection) }
   include_context 'resources'
 
