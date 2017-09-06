130c8289c99a33bb7662910ae339e9fb7705be9f
Alberto Mendoza <albertm_805@outlook.com>
2017-09-06 18:05:53 -0500

    add tests along w/ resource

--- lib/vtex/resources/specification_resource.rb
+++ lib/vtex/resources/specification_resource.rb
@@ -4,6 +4,10 @@ module Vtex
       action :specification_by_category_id, 'GET /api/catalog_system/pub/specification/field/listByCategoryId/:category_id' do
         handler(200) { |response| SpecificationMapping.extract_collection(response.body, :read) }
       end
+
+      action :specification_tree_by_category_id, 'GET /api/catalog_system/pub/specification/field/listTreeByCategoryId/:category_id' do
+        handler(200) { |response| SpecificationMapping.extract_collection(response.body, :read) }
+      end
     end
   end
 end
--- spec/lib/vtex/resources/specification_resource_spec.rb
+++ spec/lib/vtex/resources/specification_resource_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Vtex::SpecificationResource do
       expect(response.first).to be_kind_of(Vtex::Specification)
     end
   end
+
+  describe "#specification_tree_by_category_id" do
+    it "returns a list of category" do
+      stub_do_api('/api/catalog_system/pub/specification/field/listTreeByCategoryId/8', :get).to_return(body: api_fixture('specification/specification_by_category_id'))
+      response = resource.specification_tree_by_category_id(category_id: 8)
+      expect(response.first).to be_kind_of(Vtex::Specification)
+    end
+  end
 end
