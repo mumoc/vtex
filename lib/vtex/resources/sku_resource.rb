c9b87838ea1ff1b6ebcb6014df6caf7883071b7c
David Sanchez <sanchez.dav90@gmail.com>
2017-09-06 18:06:55 -0500

    Resource
    Sku List By Product Id action added to Sku Resource

--- lib/vtex/resources/sku_resource.rb
+++ lib/vtex/resources/sku_resource.rb
@@ -16,6 +16,10 @@ module Vtex
       action :sku_by_alternate_id, 'GET /api/catalog_system/pvt/sku/stockkeepingunitbyalternateId/:ean' do
         handler(200) { |response| SkuMapping.extract_single(response.body, :read) }
       end
+
+      action :sku_list_by_product_id, 'GET /api/catalog_system/pvt/sku/stockkeepingunitByProductId/:product_id' do
+        handler(200) { |response| Sku::SkuMapping.extract_collection(response.body, :read) }
+      end
     end
   end
 end
