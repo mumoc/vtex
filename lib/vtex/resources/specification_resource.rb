845a9f90b199e105595d6ecf77cafb9f64991d94
Alberto Mendoza <albertm_805@outlook.com>
2017-09-10 15:25:39 -0500

    added resources action

--- lib/vtex/resources/specification_resource.rb
+++ lib/vtex/resources/specification_resource.rb
@@ -8,6 +8,10 @@ module Vtex
       action :specification_tree_by_category_id, 'GET /api/catalog_system/pub/specification/field/listTreeByCategoryId/:category_id' do
         handler(200) { |response| SpecificationMapping.extract_collection(response.body, :read) }
       end
+
+      action :specification_field, 'GET /api/catalog_system/pub/specification/fieldGet/:field_id' do
+        handler(200) { |rsponse| SpecificationFieldMapping.extract_single(response.body, :read) }
+      end
     end
   end
 end
